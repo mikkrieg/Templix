9e5d4d41c137daa6ecb0f561550c77e1d8ffd98f
Mikey Kriegel <mikkrieg@gmail.com>
2021-05-06 11:08:59 -0700

    Add navbar with logo to header, import bootstrap to app.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 import logo from "./../img/logo.png";
+import './../Styles/App.css';
 
-function Header() {
+function Header(props) {
   return (
     <>
-      <img src={logo} alt="Templix logo"/>
+    <Container>
+      <Navbar fixed="top" id="navbar">
+        <Navbar.Brand href='#'>
+            <img src={logo} className="logo" alt='Templix logo'/>
+        </Navbar.Brand>
+      </Navbar>
+    </Container>
+      
     </>
   )
 }
