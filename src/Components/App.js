8b5ae21cec03ce1dbc83505813963030b1465837
Mikey Kriegel <mikkrieg@gmail.com>
2021-05-06 12:40:30 -0700

    Edit app.js, edit header, finish navbar, may redo logo, add custom styling for navbar

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
-import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
-import styled from 'styled-components';
+
 
 function App() {
   return (
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import Image from 'react-bootstrap/Image';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
-import logo from "./../img/logo.png";
+import Nav from 'react-bootstrap/Nav';
+import logo from './../img/logo.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './../Styles/App.css';
 
 function Header(props) {
   return (
     <>
     <Container>
-      <Navbar fixed="top" id="navbar">
+      <Navbar collapseOnSelect expand='md' fixed='top' id='navbar' className="navbar-dark">
         <Navbar.Brand href='#'>
-            <img src={logo} className="logo" alt='Templix logo'/>
+            <Image src={logo} className='logo' alt='Templix logo'/>
         </Navbar.Brand>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav'/>
+        <Navbar.Collapse id='responsive-navbar-nav' id="collapse-div">
+          <Nav id='nav-alignment'>
+            <Nav.Link href='#' className='nav-links'>Home</Nav.Link>
+            <Nav.Link href='#' className='nav-links'>About</Nav.Link>
+            <Nav.Link href='#' className='nav-links'>Layouts</Nav.Link>
+            <Nav.Link href='#' className='nav-links'>Sign up</Nav.Link>
+            <Nav.Link href='#' className='nav-links'>Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </Container>
       
