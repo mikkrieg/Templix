0d8a1ef05fe6293d0f246b4e511a428ca141cc40
Mikey Kriegel <mikkrieg@gmail.com>
2021-05-07 15:09:00 -0700

    Finish Home component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,7 +8,7 @@ export default function Home(props) {
   return(
     <>
       <div className='background background--color-map'>
-        <Container className='body-container'>
+        <Container className='home-container'>
           <h1 className='fade-in'>Welcome</h1>
           <Button onClick={() => props.layoutClick()} id='fade-button' className='background--color-map mt-5 text-color' variant='outline-dark' size='lg'>Browse Layouts</Button>
         </Container>
