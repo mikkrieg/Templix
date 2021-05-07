63ed5bcde48105be0786341ec1e650fa9236f0c7
Mikey Kriegel <mikkrieg@gmail.com>
2021-05-07 11:51:02 -0700

    Position Nav, add container for content, add styling for container

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './../Styles/Home.scss';
 
 export default function Home() {
   return(
-    <div className="background background--color-map"></div>
+    <>
+      <div className="background background--color-map">
+        <Container class="body-container">
+          <h1>Hello</h1>
+        </Container>
+      </div>
+    </>
   )
 }
