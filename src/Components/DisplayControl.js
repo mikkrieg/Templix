af5ad1e361ae958b47ccb4e8ec943e8b9180f35f
Mikey Kriegel <mikkrieg@gmail.com>
2021-05-07 10:55:43 -0700

    Add about and layout files, edit render in displayControl

--- src/Components/DisplayControl.js
+++ src/Components/DisplayControl.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import Home from './Home';
+import About from './About';
+import Layouts from './Layouts';
 
 export default class DisplayControl extends React.Component {
 
   constructor(props) {
     super(props)
     this.state = {
-      formVisible: false
+      aboutVisible: false,
+      layoutsVisible: false
     }
   }
-
   render() {
     let currentlyVisibleState = null;
-    if(this.state.formVisible != false) {
-      currentlyVisibleState = null
+    if(this.state.aboutVisible != false) {
+      currentlyVisibleState = <About/>
+
+    } else if(this.state.layoutsVisible != false) {
+      currentlyVisibleState = <Layouts />
+
     } else {
       currentlyVisibleState = <Home/>
+
     }
     return(
       <>
