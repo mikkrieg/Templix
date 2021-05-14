d767e290e880a38446f9a7d2f14e3b56845f1067
Mikey Kriegel <mikkrieg@gmail.com>
2021-05-14 11:23:57 -0700

    Add handle click function to home button

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './../Styles/Home.scss';
+import {AppContext} from './../AppContext';
 
-export default function Home(props) {
+export default function Home() {
+  const {layoutsVisible, setLayoutsVisible, setHomeVisible, setAboutVisible} = useContext(AppContext);
+  function handleLayoutsClick() {
+    if(layoutsVisible === false) {
+      setLayoutsVisible(true);
+      setHomeVisible(false)
+      setAboutVisible(false)
+    }
+  }
+
   return(
     <>
       <div className='background background--color-map'>
         <Container className='home-container'>
           <h1 className='fade-in'>Welcome</h1>
-          <Button onClick={() => props.layoutClick()} id='fade-button' className='background--color-map mt-5 text-color' variant='outline-dark' size='lg'>Browse Layouts</Button>
+          <Button onClick={() => handleLayoutsClick()} id='fade-button' className='background--color-map mt-5 text-color' variant='outline-dark' size='lg'>Browse Layouts</Button>
         </Container>
       </div>
     </>
