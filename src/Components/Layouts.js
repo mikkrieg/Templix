a57bb12d092ad7449902062a6945552f7d8e5411
Mikey Kriegel <mikkrieg@gmail.com>
2021-05-17 15:03:07 -0700

    Add more text, change image, style modal

--- src/Components/Layouts.js
+++ src/Components/Layouts.js
@@ -6,6 +6,7 @@ import Col from 'react-bootstrap/Col';
 import '../Styles/Layout.css';
 import Card from 'react-bootstrap/Card';
 import logo from '../img/logo.png'
+import layout1 from '../img/layout-1.png';
 
 
 export default function Layouts() {
@@ -34,34 +35,35 @@ export default function Layouts() {
   return(
     <div className='background background--color-map'>
       <Container className='layout-container'>
-        <Row className="layout-info">
-          <p className="layout-p">When viewing a layout, select the<span className="mark">"?"</span> for instructions and specifications</p>
+        <Row className='layout-info'>
+          <p className='layout-p'>When viewing a layout, select the<span className='mark'>'?'</span> for instructions and specifications</p>
+          <p className='layout-p'>Downloads are currently only available on Desktop</p>
         </Row>
-        <Row className="mt-5 card-row">
+        <Row className='mt-5 card-row'>
           <Col lg={4}>
-            <Card onClick={() => handleCard1Click()} className="card-styles" id="card-1">
-              <Card.Img variant="top" src={logo}/>
+            <Card onClick={() => handleCard1Click()} className='card-styles' id='card-1'>
+              <Card.Img variant='top' src={layout1}/>
               <Card.Body>
                 <Card.Title>Layout 1</Card.Title>
-                <Card.Text className="card-text">lorem ipsum about this layout its completley random placeholder txt bruh</Card.Text>
+                <Card.Text className='card-text'>This is a layout styled with react-boostrap and css modules that provides a woodland theme.</Card.Text>
               </Card.Body>
             </Card>
           </Col>
           <Col lg={4}>
-          <Card onClick={() => handleCard2Click()} className="card-styles" id="card-2">
-              <Card.Img variant="top" src={logo}/>
+          <Card onClick={() => handleCard2Click()} className='card-styles' id='card-2'>
+              <Card.Img variant='top' src={logo}/>
               <Card.Body>
                 <Card.Title>Layout 2</Card.Title>
-                <Card.Text className="card-text">lorem ipsum about this layout its completley random placeholder txt bruh</Card.Text>
+                <Card.Text className='card-text'>lorem ipsum about this layout its completley random placeholder txt bruh</Card.Text>
               </Card.Body>
             </Card>
           </Col>
           <Col lg={4}>
-          <Card onClick={() => handleCard3Click()} className="card-styles" id="card-3">
-              <Card.Img variant="top" src={logo}/>
+          <Card onClick={() => handleCard3Click()} className='card-styles' id='card-3'>
+              <Card.Img variant='top' src={logo}/>
               <Card.Body>
                 <Card.Title>Layout 3</Card.Title>
-                <Card.Text className="card-text">lorem ipsum about this layout its completley random placeholder txt bruh</Card.Text>
+                <Card.Text className='card-text'>lorem ipsum about this layout its completley random placeholder txt bruh</Card.Text>
               </Card.Body>
             </Card>
           </Col>
